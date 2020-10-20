f8fc6c206d7182237a3c5f93e61d5cd4d8828f11
Leonardo Rodrigues Ribeiro <leorr112624@gmail.com>
2020-10-20 14:14:31 -0300

    2020/10/20 14:14 Implementacao css melhorado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,35 +4,47 @@ import './App.css';
 import Pedidos from './Pedidos';
 
 export default class PersonList extends React.Component {
+
   state = {
     cpf: '',
     pedidos: [],
   }
+
   handleChange = event => {
     this.setState({ cpf: event.target.value });
   }
+
   handleSubmit = event => {
+
     event.preventDefault();
 
-    axios.get(`http://192.168.0.11:8080/api/pedidos/`+this.state.cpf)
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-        this.setState({ pedidos: res.data });
-      })
+    if(this.state.cpf === "")
+      this.setState({pedidos: []});
+    else
+      axios.get(`http://192.168.0.11:8080/api/pedidos/`+ this.state.cpf)
+        .then(res => {
+          this.setState({ pedidos: res.data });
+        })
   }
+
   render() {
     return (
-      <div className="center">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            CPF:
-            <input type="text" name="cpf" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Busca</button>
+      <div className="center maindiv bg">
+        <h1 className="centerTittle">
+        Rastreie suas encomendas</h1>
+        <form onSubmit={this.handleSubmit} className="center">
+          <div className="centerTittle">
+            <input type="text" name="cpf_input" placeholder="Digite seu cpf" onChange={this.handleChange}className="center input"/>
+            <button type="submit" className="button">🔍</button>
+          </div>
         </form>
         <Pedidos pedidos={this.state.pedidos} />
-      </div>
+        <footer>
+          <p>Autor: Leonardo Ribeiro<br/>
+          <a href="https://github.com/leorr">Repositorios Github</a></p>
+        </footer>
+
+        </div>
     )
   }
 }
--- frontend/src/Pedidos.js
+++ frontend/src/Pedidos.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import './App.css'
 
 const Pedidos = ({pedidos}) => {
-return (
-        <div>
-          <center><h1>Pedido List</h1></center>
-          {pedidos.map((pedido) => (
-            <div className="pedido">
-              <div className="pedido-body">
-                <h5 className="pedido-id">Pedido ID: {pedido.id}</h5>
-                <h4 className="pedido-cliente">Cliente: {pedido.cliente.nome} <br />
-                  Endereço: {pedido.endereco.rua}, {pedido.endereco.numero} - {pedido.endereco.bairro}<br />
-                  Situação: {pedido.status} <br />
-                </h4>
-              </div>
-         </div>
-          ))}
-        </div>
+    if(pedidos && pedidos.length)
+        return (
+            <div className="listapedidos">
+              {pedidos.map((pedido) => (
+                <div className="pedido">
+                  <div className="pedido-body padd">
+                    <h5 className="pedido-id">Pedido ID: {pedido.id}</h5>
+                          <h4>Destinatário:<span className='notbold'> {pedido.cliente.nome}</span> <br />
+                                  Endereço:<span className='notbold'> {pedido.endereco.rua}, {pedido.endereco.numero} - {pedido.endereco.bairro}</span><br />
+                                  Situação:<span className='notbold'> {pedido.status} em {pedido.data}</span> <br />
+                    </h4>
+                  </div>
+             </div>
+              ))}
+            </div>
       )
+    else
+        return(
+            <div/>
+        )
 };
 
 export default Pedidos;
